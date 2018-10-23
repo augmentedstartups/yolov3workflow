67ee83802760e9a884f42acced7e39b8c04c9abd
Augmented Startups <reigngt09@gmail.com>
2018-10-23 21:19:38 +0200

    Updated for over 100 images

--- 1_WebImage_Scraping/image_download.py
+++ 1_WebImage_Scraping/image_download.py
@@ -2,6 +2,7 @@ from google_images_download import google_images_download   #importing the libra
 
 response = google_images_download.googleimagesdownload()   #class instantiation
 
-arguments = {"keywords":"Honey Bottle,honey,honey squeeze","limit":20,"print_urls":True}   #creating list of arguments
+arguments = {"keywords":"Honey Bottle,honey,honey squeeze","limit":111,"chromedriver":"C:\\Drivers\\chromedriver\\chromedriver.exe","print_urls":True}   #creating list of arguments
+#Make sure to set chromedrive is the appropriate director if you want to download more than 100 images
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
